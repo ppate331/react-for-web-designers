c54588ee6765562b58b832d13422ecbd06e92aa3
Prachal Patel <ppate331@uic.edu>
2018-05-05 01:51:55 +0000

    chapter 2.1 done

--- Exercise Files/Ch02/02_01/start/app.js
+++ Exercise Files/Ch02/02_01/start/app.js
@@ -2,5 +2,9 @@
   "use strict";
 
   // Start here
+  
+  var ProductCustomizer = React.createElement('div', { className: "customizer" },"product customizer will go here" );
+  
+  ReactDOM.render(ProductCustomizer, document.getElementById('react-root'));
 
 })();
