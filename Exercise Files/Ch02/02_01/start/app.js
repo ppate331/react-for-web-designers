60bc982c415af46773589680a9faf3921ced5c57
Prachal Patel <ppate331@uic.edu>
2018-05-05 03:09:28 +0000

    chapter 2.4 completed

--- Exercise Files/Ch02/02_01/start/app.js
+++ Exercise Files/Ch02/02_01/start/app.js
@@ -6,28 +6,22 @@
   function ProductImage(props){
    
     
-    return React.createElement("img", {
-      src:"../../../assets/red.jpg", 
-      alt: "Product image"
-      
-    });
+   return <img src="../../../assets/red.jpg" alt="Product Image" />;
   }
   
-    function ProductCustomizer(props) {
-      
-     return React.createElement(
-       "div", 
-       { className: "customizer" },
-       React.createElement("div", { className: "product-image" }, 
-       React.createElement(ProductImage)
-       )
+  function ProductCustomizer(props) {
+    return (
+      <div className="customizer">
+      <div className="product-image">
+        <ProductImage />
+      </div>
+      </div>
     );
-     
   }
 
   
-  
-  ReactDOM.render(React.createElement(ProductCustomizer), document.getElementById('react-root'));
-  
 
+  ReactDOM.render(<ProductCustomizer />, document.getElementById("react-root"));
+  
+  
 })();
